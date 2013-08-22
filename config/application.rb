09e3c72b750b60d0334b5f4396e189491e530891
Prashant Dang <prashantdang@gmail.com>
2013-08-22 22:38:23 +0200

    Add user Model with Devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,10 @@ module Omrails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Reqired for Devise on Heroku
+    config.assets.initialize_on_precompile = false
+
+
   end
 end
